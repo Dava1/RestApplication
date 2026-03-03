d59575ffb34a3d8d1f4fab631d8be35727b76565
Dava1 <se_fi_rot@mail.ru>
2026-03-03 21:36:23 +0100

    added path updated and validation

--- src/main/java/com/train/rest/configuration/WebConfig.java
+++ src/main/java/com/train/rest/configuration/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Bean
-	public void addLoggingFilter(InterceptorRegistry registry){
+	@Override
+	public void addInterceptors(InterceptorRegistry registry){
 		registry.addInterceptor(new LoggingInterceptor());
 	}
 }
